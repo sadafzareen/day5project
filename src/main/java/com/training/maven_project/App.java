6bd9ff92de4a38bdc90c85e468b1decaa17385c6
sadafzareen <zareensadaf56@gmail.com>
2022-10-12 11:52:03 +0000

    Initial

--- src/main/java/com/training/maven_project/App.java
+++ src/main/java/com/training/maven_project/App.java
@@ -1,5 +1,7 @@
 package com.training.maven_project;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
@@ -7,7 +9,12 @@ package com.training.maven_project;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    { 
+    	TreeMap<Integer,String>treemap=new TreeMap<Integer,String>();
+    	treemap.put(4,"Monday");
+    	treemap.put(3,"Tuesday");
+    	treemap.put(2,"Wednesday");
+    	treemap.put(1,"Thursday");
+        System.out.println( "Descending map:"+treemap.descendingMap());
     }
 }
